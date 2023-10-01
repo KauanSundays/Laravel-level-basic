9e4ef79b1c5bd7e2569f6b2f37f8198e24716c94
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-10-01 00:04:59 -0300

    feat: wip

--- Rotas e Controladores/1.Crie uma rota que retorna uma mensagem simples/resources/views/livewire/clicker.blade.php
+++ Rotas e Controladores/1.Crie uma rota que retorna uma mensagem simples/resources/views/livewire/clicker.blade.php
@@ -1,3 +1,7 @@
 <div>
-    {{-- Be like water. --}}
+    <form action="">
+        <input wire:model="name" type="text" placeholder="name">
+        <input wire:model="email" type="email" placeholder="email">
+        <input wire:model="password" type="password" placeholder="password">
+    </form>
 </div>
