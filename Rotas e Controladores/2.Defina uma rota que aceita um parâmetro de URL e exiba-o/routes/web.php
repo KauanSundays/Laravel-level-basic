9f2cb8a233fd1a88f58a3615d31a3289f6f30386
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-10-02 22:13:43 -0300

    feat: wip

--- Rotas e Controladores/2.Defina uma rota que aceita um parâmetro de URL e exiba-o/routes/web.php
+++ Rotas e Controladores/2.Defina uma rota que aceita um parâmetro de URL e exiba-o/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/simple', function () {
+    return view('simple');
+})->name('simple_route');
