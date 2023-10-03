0a037f8725029a3588ed9dd36517aa96aa8d7b3d
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-10-02 21:20:57 -0300

    feat: wip

--- Rotas e Controladores/1.Crie uma rota que retorna uma mensagem simples/routes/web.php
+++ Rotas e Controladores/1.Crie uma rota que retorna uma mensagem simples/routes/web.php
@@ -17,10 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/created-route', function () {
-<<<<<<< HEAD
-    return view('livewire/clicker');  // this route returns a string, 
-=======
-    return view('teste');  // this route returns a string, 
->>>>>>> 9e4ef79b1c5bd7e2569f6b2f37f8198e24716c94
+Route::get('/simple', function () {
+    return view('simple');
 });
