f7f41fb88a1c5637ff0af3e4d654d257a258e490
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-10-11 11:33:57 -0300

    feat: wip

--- Modelos e Banco de Dados/database/migrations/2023_10_11_142230_create_produtos_table.php
+++ Modelos e Banco de Dados/database/migrations/2023_10_11_142230_create_produtos_table.php
@@ -4,24 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProdutosTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('produtos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); // Coluna de ID auto-incremento
+            $table->string('nome'); //nome do produto
+            $table->decimal('preco', 10, 2); 
+            //decimal, 
+            //10 -> Numero maximo de numero antes da virgula
+            //2 -> Numero maximo de numeros depois da virgula
+
+            $table->timestamps(); // Colunas created_at e updated_at para registro de data e hora
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('produtos');
     }
-};
+}
