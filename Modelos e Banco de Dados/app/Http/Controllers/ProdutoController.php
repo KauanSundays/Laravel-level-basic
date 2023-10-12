683158fe73f9b01b6e677f85128b5f065ee8cebb
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-10-12 06:37:26 -0300

    feat: wip

--- Modelos e Banco de Dados/app/Http/Controllers/ProdutoController.php
+++ Modelos e Banco de Dados/app/Http/Controllers/ProdutoController.php
@@ -9,7 +9,7 @@ class ProdutoController extends Controller
 {
     public function listarProdutos() {
         $produtos = Produto::all();
-    
-        return view('welcome', ['produtos' => $produtos]);
+        return view('welcome', ['produtos' => $produtos]);    
+    //Variavel $produtos contem todos os registros da tabela produtos, passados para a view
     }
 }
