77cbcfd35766f79ceba085fd8dc7b1b7136fca28
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-10-12 06:38:59 -0300

    feat: wip

--- Modelos e Banco de Dados/routes/web.php
+++ Modelos e Banco de Dados/routes/web.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ProdutoController::class, 'listarProdutos']);
+
+Route::get('/produtos/criar', [ProdutoController::class, 'criarProdutos']);
