5ea7e666e93f8ee856b9735c219d1af30e171d9a
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-02 11:00:22 -0300

    feat: wip

--- Formularios-Validacoes/app/Http/Kernel.php
+++ Formularios-Validacoes/app/Http/Kernel.php
@@ -14,10 +14,9 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        \App\Http\Middleware\SayCheeseMiddleware::class,
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Illuminate\Http\Middleware\HandleCors::class,  
+        \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -36,7 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,   
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
         'api' => [
