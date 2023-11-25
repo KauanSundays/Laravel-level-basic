6761e24120164551de1ae7742567457091bbcf83
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-25 12:50:13 -0300

    wip

--- Formularios/routes/web.php
+++ Formularios/routes/web.php
@@ -5,13 +5,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $produtos = Produto::all(); // Obtém todos os produtos do banco de dados
+    return view('welcome', ['produtos' => $produtos]);
 });
 
 Route::post('/cadastrar-produto', function (\Illuminate\Http\Request $request) {
     $produto = new Produto();
     $produto->nome = $request->input('nome');
-    $produto->preco_custo = 0; // Coloque o valor desejado
+    $produto->preco_custo = $request->input('preco_custo') ; 
     $produto->disponivel = $request->input('disponivel');
     $produto->save();
 
