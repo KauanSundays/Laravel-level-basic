b3bf455639257e484b92e5b8af98f41a66d44541
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-26 08:49:07 -0300

    wip

--- Formularios/app/Http/Controllers/ProdutosController.php
+++ Formularios/app/Http/Controllers/ProdutosController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produto;
 use Illuminate\Http\Request;
 
 class ProdutosController extends Controller
 {
-    //
+    public function index()
+    {
+        $produtos = Produto::all();
+        return view('welcome', ['produtos' => $produtos]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required|string|max:255',
+            'preco_custo' => 'required|numeric|min:0',
+            'disponivel' => 'required|in:sim,nao',
+        ]);
+
+        $produto = new Produto();
+        $produto->nome = $request->input('nome');
+        $produto->preco_custo = $request->input('preco_custo');
+        $produto->disponivel = $request->input('disponivel');
+        $produto->save();
+
+        return redirect('/');
+    }
 }
--- Formularios/routes/web.php
+++ Formularios/routes/web.php
@@ -3,18 +3,7 @@
 use App\Models\Produto;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutosController;
 
-Route::get('/', function () {
-    $produtos = Produto::all(); // Obtém todos os produtos do banco de dados
-    return view('welcome', ['produtos' => $produtos]);
-});
-
-Route::post('/cadastrar-produto', function (\Illuminate\Http\Request $request) {
-    $produto = new Produto();
-    $produto->nome = $request->input('nome');
-    $produto->preco_custo = $request->input('preco_custo') ; 
-    $produto->disponivel = $request->input('disponivel');
-    $produto->save();
-
-    return redirect('/');
-})->name('cadastrar.produto');
+Route::get('/', [ProdutosController::class, 'index']);
+Route::post('/cadastrar-produto', [ProdutosController::class, 'store'])->name('cadastrar.produto');
