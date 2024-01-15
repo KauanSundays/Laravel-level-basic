e18b1e8b74be0dd7a54f894a7a2bbf2614ebfb46
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-01-15 17:02:14 -0300

    error resolve

--- Relacionamentos/routes/web.php
+++ Relacionamentos/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
 
-Route::get('/', [PostsController::class, 'index']);
+Route::get('/', [PostController::class, 'index']);
