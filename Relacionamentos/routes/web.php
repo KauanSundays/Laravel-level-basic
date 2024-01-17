84ea6fa8354dc98578274f4d8845e056cde29235
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-01-16 21:14:05 -0300

    wip

--- Relacionamentos/routes/web.php
+++ Relacionamentos/routes/web.php
@@ -1,22 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
-use App\Models\Post;
-use App\Models\Comment;
-use Illuminate\Http\Request;
-
-class PostController extends Controller
-{
-    public function index()
-    {
-        $posts = Post::all();
-        return view('welcome', ['posts' => $posts]);
-    }
-
-    public function show(Post $post)
-    {
-        $comments = Comment::where('post_id', $post->id)->get();
-        return view('posts.show', ['post' => $post, 'comments' => $comments]);
-    }
-}
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
