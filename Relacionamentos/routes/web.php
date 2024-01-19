83e99180c7753d0987f9450764cee6dff5f2d923
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-01-18 21:48:30 -0300

    controller changes

--- Relacionamentos/routes/web.php
+++ Relacionamentos/routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentsController;
+
 
 Route::get('/', [PostController::class, 'index']);
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::post('/comments/{postId}', [CommentsController::class, 'store'])->name('comments.store');
