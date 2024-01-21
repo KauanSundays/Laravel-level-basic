40c730426a2c2ead0413ac37877d94b0833d498e
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-01-21 09:36:19 -0300

    wip

--- Paginacao/database/factories/PostFactory.php
+++ Paginacao/database/factories/PostFactory.php
@@ -3,12 +3,15 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Post;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
  */
 class PostFactory extends Factory
 {
+    protected $model = Post::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +20,9 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->text(),
+            'slug' => Str::slug($this->faker->text()),
+            'body'=> $this->faker->paragraph()
         ];
     }
 }
