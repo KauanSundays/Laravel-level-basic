eb070c6789c89b8cc2594df493401d95068f286d
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-01-23 19:59:25 -0300

    wip

--- EnviarEmails/app/Http/Controllers/ContactController.php
+++ EnviarEmails/app/Http/Controllers/ContactController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function store()
+    {
+        
+    }
 }
